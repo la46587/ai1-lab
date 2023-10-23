0b601ec8b29d167d34906492bb57291ca29fc352
la46587 <la46587@zut.edu.pl>
2023-10-23 23:52:07 +0200

    chyba gotowe

--- lab-b/listScript.js
+++ lab-b/listScript.js
@@ -2,14 +2,15 @@ let Todo = {
     tasks: [],
     date: [],
     status: [],
-    term: '',
+    term: document.getElementById('search'),
     draw: function() {
         Todo.sortList();
-        Todo.getFilteredTasks();
         const ul = document.getElementById('list');
+        ul.id = 'list';
         ul.innerHTML = '';
         for (let i = 0; i < Todo.tasks.length; i++) {
             let li = document.createElement('li');
+            li.id = 'listElement'
             li.className = Todo.status[i];
             ul.appendChild(li);
 
@@ -24,7 +25,7 @@ let Todo = {
                 paraEdit.value = Todo.tasks[i];
                 paraEdit.onblur = function() {
                     if (paraEdit.value === '' || paraEdit.value.length < 3 || paraEdit.value.length > 255 ||
-                        paraEdit.value.charAt(0) === ' ' || paraEdit.value.charAt(paraEdit.value.length - 1) === ' ')
+                    paraEdit.value.charAt(0) === ' ' || paraEdit.value.charAt(paraEdit.value.length - 1) === ' ')
                         alert('Treść zadania musi mieć między 3 a 255 znaków i nie może zaczynać ani kończyć się spacją.')
                     else
                         Todo.tasks[i] = paraEdit.value;
@@ -51,7 +52,7 @@ let Todo = {
                 timeEdit.value = Todo.date[i];
                 timeEdit.onblur = function() {
                     if (timeEdit.value === '')
-                        Todo.date[i] = 'bezterminowe'
+                        Todo.date[i] = 'bezterminowo'
                     else
                         Todo.date[i] = timeEdit.value;
                     li.removeChild(timeEdit);
@@ -106,28 +107,32 @@ let Todo = {
             li.append(closeBtn);
             closeBtn.append(close);
 
+            // Zapisanie do localStorage
             localStorage.setItem('tasks', JSON.stringify(Todo.tasks));
             localStorage.setItem('date', JSON.stringify(Todo.date));
             localStorage.setItem('status', JSON.stringify(Todo.status));
         }
+        if (Todo.term.value)
+            Todo.getFilteredTasks();
     },
     // Dodawanie zadania
     addTask: function() {
         const todaysDate = new Date().toJSON().slice(0, 10);
         let inputText = document.getElementById('add');
         let inputDate = document.getElementById('addDate');
-        if (inputText.value.length >= 3 && inputText.value.length <= 255 && inputText.value.charAt(0) !== ' ' && inputText.value.charAt(inputText.value.length - 1) !== ' ') {
+        if (inputText.value.length >= 3 && inputText.value.length <= 255 && inputText.value.charAt(0) !== ' ' &&
+        inputText.value.charAt(inputText.value.length - 1) !== ' ') {
             if (inputDate.value >= todaysDate || inputDate.value === ''){
                 Todo.tasks.push(inputText.value);
                 if (inputDate.value === '')
-                    Todo.date.push('bezterminowe');
+                    Todo.date.push('bezterminowo');
                 else
                     Todo.date.push(inputDate.value)
                 Todo.status.push('unchecked');
                 inputText.value = '';
                 inputDate.value = '';
-                Todo.loseFocus('add');
                 Todo.loseFocus('addDate');
+                Todo.getFocus('add');
                 Todo.draw();
             }
             else
@@ -141,7 +146,7 @@ let Todo = {
         const limitlessTasks = [];
         const limitlessStatus = [];
         for (let i = 0; i < Todo.date.length; i++) {
-            if (Todo.date[i] === 'bezterminowe') {
+            if (Todo.date[i] === 'bezterminowo') {
                 limitlessDate.push(Todo.date[i]);
                 limitlessTasks.push(Todo.tasks[i]);
                 limitlessStatus.push(Todo.status[i]);
@@ -189,19 +194,44 @@ let Todo = {
         Todo.date = Todo.date.concat(checkedDate);
         Todo.status = Todo.status.concat(checkedStatus);
     },
+    // Wyszukiwanie
     getFilteredTasks: function() {
-        let inputSearch = document.getElementById('search');
-        // console.log(inputSearch.value);
+        let filter = Todo.term.value.toUpperCase();
+        let filterNoCap = Todo.term.value.slice();
+        let ul = document.getElementById('list');
+        let li = ul.getElementsByTagName('li');
+        for (let i = 0; i < Todo.tasks.length; i++) {
+            let p = li[i].firstChild;
+            let pText = p.innerHTML;
+            if (Todo.tasks[i].toUpperCase().includes(filter)) {
+                li[i].style.display = '';
+                pText = Todo.tasks[i];
+                if (filter !== '')
+                    pText = Todo.highlight(pText, filterNoCap);
+                li[i].firstChild.innerHTML = pText;
+            } else {
+                li[i].style.display = 'none';
+            }
+            if (filter === '')
+                Todo.draw();
+        }
     },
+    // Podświetlenie wyszukiwanej frazy
+    highlight: function(text, filter) {
+        let replacement = new RegExp(filter, 'gi');
+        return text.replace(replacement, `<mark>$&</mark>`)
+    },
+    // Focus na polu do wprowadzania
     getFocus: function(id) {
         document.getElementById(id).focus();
     },
+    // Utrata focusu na polu do wprowadzania
     loseFocus: function(id) {
         document.getElementById(id).blur();
     }
 }
 
-// Dodawanie zadania enterem
+// Dodawanie zadania i daty enterem
 const addTextField = document.getElementById('add');
 const dateField = document.getElementById('addDate');
 addTextField.addEventListener('keydown', function(event) {
